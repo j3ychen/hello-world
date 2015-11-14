ccf1a1e4938a85705ebf3edaf452661fd917efc5
Jerry Chen <jychen216@gmail.com>
2015-11-13 21:25:09 -0800

    Update test.php

--- php/test.php
+++ php/test.php
@@ -6,9 +6,9 @@
 function makeName() {
   $first = array("Buzz", "Snap", "Pay", "Bit", "Square");
   $second = array("X", "Hub", "Wire", "DB", "Ware", "Labs");
-  $firstrand = int rand (0, count($first));
-  $secondrand = int rand (0, count($second));
-  echo $first[$firstrand] . $second[$secondrand];
+  $firstrand = int rand (0, count($first-1));
+  $secondrand = int rand (0, count($second-1));
+  echo <h1>$first[$firstrand] . $second[$secondrand]</h1>;
 }
 ?>
 
