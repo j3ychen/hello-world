8af7062ba99d6be5453bcb54df4622b05695b018
Jerry Chen <jychen216@gmail.com>
2015-11-14 09:41:03 -0800

    Update test.php

--- php/test.php
+++ php/test.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
+$coname = "BuzzX";
 function makeName() {
   $first = array("Buzz", "Snap", "Pay", "Bit", "Square");
   $second = array("X", "Hub", "Wire", "DB", "Ware", "Labs");
   $firstrand = int rand (0, count($first-1));
   $secondrand = int rand (0, count($second-1));
-  echo <h1>$first[$firstrand] . $second[$secondrand]</h1>;
+  global $coname = $first[$firstrand] . $second[$secondrand];
 }
+$doc->getElementById('co-name')->$coname;
 ?>
-
-</body>
-</html>
